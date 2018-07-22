cf6c9937744360485c93fa6e0a419aaa7bb46e74
hahwul <hahwul@gmail.com>
2018-07-22 23:03:01 +0900

    코드 리팩토링

--- ftc.rb
+++ ftc.rb
@@ -1,30 +1,30 @@
 require 'clipboard'
 
 def read_file(file_name)
-  file = File.open(file_name, "r")
+  file = File.open(file_name, 'r')
   data = file.read
   file.close
-  return data
+  data
 end
 
 def usage
-  puts "Usage:: ftc.rb [file]"
+  puts 'Usage:: ftc.rb [file]'
 end
 
-#[TODO] Argument parsing?
+# [TODO] Argument parsing?
 #       -h, --help, -v, --version?
 
 argument = ARGV
-if argument.size == 0
-  usage()
-  exit()
+if argument.empty?
+  usage
+  exit
 end
 
-#[TODO] Add multi argument and copy (multi copy)
+# [TODO] Add multi argument and copy (multi copy)
 
 fname = argument.pop
 data = read_file(fname)
 
 Clipboard.copy(data)
 
-puts "[+] Success copy "+data.length.to_s+" length data to clipboard"
+puts '[+] Success copy ' + data.length.to_s + ' length data to clipboard'
