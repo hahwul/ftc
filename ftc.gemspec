00e5fffb71ee5f9a62a2f289b0d61a1730f62218
hahwul <hahwul@gmail.com>
2018-10-04 01:03:21 +0900

    add gem

---
+++ ftc.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'ftc'
+  s.version     = '0.0.1'
+  s.date        = '2018-04-28'
+  s.summary     = "file to clipboard"
+  s.description = "File to Clipboard gem"
+  s.authors     = ["hahwul"]
+  s.email       = 'hahwul@gmail.com'
+  s.files       = ["lib/ftc.rb"]
+  s.homepage    =
+    'https://www.hahwul.com'
+  s.license       = 'MIT'
+end
---
+++ lib/ftc.rb
@@ -0,0 +1,30 @@
+require 'clipboard'
+
+def read_file(file_name)
+  file = File.open(file_name, 'r')
+  data = file.read
+  file.close
+  data
+end
+
+def usage
+  puts 'Usage:: ftc.rb [file]'
+end
+
+# [TODO] Argument parsing?
+#       -h, --help, -v, --version?
+
+argument = ARGV
+if argument.empty?
+  usage
+  exit
+end
+
+# [TODO] Add multi argument and copy (multi copy)
+
+fname = argument.pop
+data = read_file(fname)
+
+Clipboard.copy(data)
+
+puts '[+] Success copy ' + data.length.to_s + ' length data to clipboard'
